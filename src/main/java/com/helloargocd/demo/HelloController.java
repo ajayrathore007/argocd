0aef7aea324cd60282e3bd1d4dc4a7318a628e2d
ajayrathore007 <121972278+ajayrathore007@users.noreply.github.com>
2023-01-06 01:49:39 +0530

    Update HelloController.java

--- src/main/java/com/helloargocd/demo/HelloController.java
+++ src/main/java/com/helloargocd/demo/HelloController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    Logger logger = Logger.getLogger(String.valueOf(HelloController.class));
+    
     @GetMapping("/")
     public String helloArgo(){
+        logger.info("Endpoint");
         return "Hello Argo";
     }
 }
